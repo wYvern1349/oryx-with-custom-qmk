b6861379fd779ee4acbe59dfc1726205eb666880
wYvern1349 <111669527+wYvern1349@users.noreply.github.com>
2025-02-17 00:10:43 +0100

    Update keymap.c

--- XlJZ9/keymap.c
+++ XlJZ9/keymap.c
@@ -266,7 +266,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case ARCANE_SFT: 
                if (record->event.pressed) {
                  if (get_oneshot_mods() & MOD_MASK_SHIFT) {
-                   caps_word_on();//register_code(KC_LSFT);
+                   caps_word_toggle();//register_code(KC_LSFT);
                //} else if (get_mods() & MOD_MASK_SHIFT) {
                  //  unregister_code(KC_LSFT);
                } else {
