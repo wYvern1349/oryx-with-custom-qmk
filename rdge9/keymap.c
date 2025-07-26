e95b63796caea8f866fbf39ad45ed12b2b21bba5
wYvern1349 <111669527+wYvern1349@users.noreply.github.com>
2025-07-26 12:34:08 +0200

    Update keymap.c

--- rdge9/keymap.c
+++ rdge9/keymap.c
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     TO(2),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TO(2),          
     KC_TRANSPARENT, OSL(5),         KC_L,           KC_D,           KC_M,           KC_F,                                           KC_Y,           KC_P,           KC_O,           KC_U,           ST_MACRO_0,     KC_TRANSPARENT, 
-    KC_ENTER,       KC_N,           KC_R,           KC_T,           KC_S,           KC_C,                                           KC_B,           KC_H,           KC_E,           KC_I,           KC_A,           RCTL(KC_BSPC),  
-    TO(1),          KC_X,           KC_J,           KC_G,           KC_W,           KC_V,                                           KC_Z,           KC_K,           OSL(5),         KC_COMMA,       KC_DOT,         TO(1),          
+    KC_ENTER,       KC_N,           KC_R,           KC_T,           KC_S,           KC_C,                                           KC_K,           KC_H,           KC_E,           KC_I,           KC_A,           RCTL(KC_BSPC),  
+    TO(1),          KC_X,           KC_J,           KC_G,           KC_W,           KC_V,                                           KC_Z,           KC_B,           OSL(5),         KC_COMMA,       KC_DOT,         TO(1),          
                                                     ARCANE_L, OSL(4),                                         OSL(3),         KC_SPACE
   ),
   [1] = LAYOUT_voyager(
