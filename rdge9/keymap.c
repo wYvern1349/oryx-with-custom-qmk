4473739ec9f1d48481715ac036aea5c366315a13
wYvern1349 <111669527+wYvern1349@users.noreply.github.com>
2025-08-01 12:56:58 +0200

    Update keymap.c

--- rdge9/keymap.c
+++ rdge9/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, OSL(5),         KC_L,           KC_D,           KC_M,           KC_V,                                           KC_Y,           KC_P,           KC_O,           KC_U,           ST_MACRO_0,     KC_TRANSPARENT, 
     KC_ENTER,       KC_R,           KC_N,           KC_T,           KC_S,           KC_F,                                           KC_C,           KC_H,           KC_E,           KC_I,           KC_A,           RCTL(KC_BSPC),  
     TO(1),          KC_Z,           KC_J,           KC_G,           KC_W,           KC_X,                                           KC_K,           KC_B,           OSL(5),         KC_COMMA,       KC_DOT,         TO(1),          
-                                                    ARCANE_L, OSL(4),                                         OSL(3),         KC_SPACE
+                                                    KC_TRANSPARENT, OSL(4),                                         OSL(3),         KC_SPACE
   ),
   [1] = LAYOUT_voyager(
     KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_F5,          
